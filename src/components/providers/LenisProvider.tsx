b44886071eed6bfd5c515d36c88134ca6ae0bc15
Mitya <mitywaflow@gmail.com>
2026-09-05 21:05:37 +0200

    added smooth-scroll(Lenis) to landing
    Signed-off-by: Mitya <mitywaflow@gmail.com>

---
+++ src/components/providers/LenisProvider.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { ReactNode, useEffect } from "react";
+import Lenis from "@studio-freight/lenis";
+
+declare global {
+    interface Window {
+        lenis: Lenis;
+    }
+}
+type Props = {
+    children: ReactNode;
+};
+
+//export lenis best set
+
+export default function LenisProvider({ children }: Props) {
+    useEffect(() => {
+        const lenis = new Lenis({
+            lerp: 0.08,
+            wheelMultiplier: 1,
+            touchMultiplier: 1,
+        });
+
+        window.lenis = lenis;
+
+        function raf(time: number) {
+            lenis.raf(time);
+            requestAnimationFrame(raf);
+        }
+
+        requestAnimationFrame(raf);
+
+        return () => {
+            lenis.destroy();
+        };
+    }, []);
+
+    return <>{children}</>;
+}
+
+// "use client";
+
+// import { useEffect } from "react";
+// import Lenis from "lenis";
+
+// export default function LenisProvider({
+//     children,
+// }: {
+//     children: React.ReactNode;
+// }) {
+//     useEffect(() => {
+//         const lenis = new Lenis({
+//             smoothWheel: true,
+//         });
+
+//         function raf(time: number) {
+//             lenis.raf(time);
+//             requestAnimationFrame(raf);
+//         }
+
+//         requestAnimationFrame(raf);
+
+//         return () => {
+//             lenis.destroy();
+//         };
+//     }, []);
+
+//     return children;
+// }
